38e301884d34522448d6e6198e02778df0b790f0
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-08-26 11:57:24 +0800

    Implement basic sign-up page

--- lib/presentation/sign_up/pages/sign_up_page.dart
+++ lib/presentation/sign_up/pages/sign_up_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:up_companion_app/utils/constants/up_campuses.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -9,11 +10,126 @@ class SignUpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Sign-Up')),
-      body: Center(
-        child: Form(
-            child: Column(
-          children: const <Widget>[],
-        )),
+      body: const SignUpForm(),
+    );
+  }
+}
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  SignUpFormState createState() => SignUpFormState();
+}
+
+class SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Username',
+              ),
+              keyboardType: TextInputType.name,
+              textInputAction: TextInputAction.next,
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+              ),
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+            ),
+            const SizedBox(height: 15),
+            DropdownButtonFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.only(
+                  top: 18,
+                  right: 18,
+                  bottom: 18,
+                  left: 12,
+                ),
+                labelText: 'UP Campus',
+              ),
+              items: <String>[
+                'UP Diliman',
+                'UP Los Baños',
+                'UP Manila',
+                'UP Visayas',
+                'UP Open University',
+                'UP Mindanao',
+                'UP Baguio',
+                'UP Cebu',
+              ].map<DropdownMenuItem<String>>((String upCampusName) {
+                return DropdownMenuItem(
+                  value: upCampusName,
+                  child: Text(upCampusName),
+                );
+              }).toList(),
+              onChanged: (upCampusName) {
+                print(upCampusName);
+              },
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_right_rounded),
+              label: const Text('Create My Account'),
+            ),
+            const SizedBox(height: 15),
+            Column(
+              children: <Widget>[
+                const Text(
+                  'By creating an account, you agree to our',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 11,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Privacy Policy',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 11,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      ' and ',
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 11,
+                      ),
+                    ),
+                    Text(
+                      'Terms and Conditions.',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 11,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
